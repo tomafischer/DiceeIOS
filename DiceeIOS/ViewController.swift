84b63e134311e8dca9330612341e5edfe367d2eb
Tom <tom.a.fischer@gmail.com>
2019-05-24 14:28:05 -0400

    Initial Commit

---
+++ DiceeIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceeIOS
+//
+//  Created by tom on 5/24/19.
+//  Copyright © 2019 com.aft3000. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
